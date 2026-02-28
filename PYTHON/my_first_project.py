20915f009a64afcdd9cd46236910f648681e8411
vikram461 <vikramchavan461@gmail.com>
2026-03-01 02:43:50 +0530

    Initial commit

---
+++ PYTHON/my_first_project.py
@@ -0,0 +1,14 @@
+"""
+PEP 8 : recommends every file should start with a docstring.
+PEP 8 : (Python's style guide) requires 2 blank lines between the module-level
+      docstring and function definitions at the top level of a file.
+"""
+
+
+def main():
+    """This is the main function."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
